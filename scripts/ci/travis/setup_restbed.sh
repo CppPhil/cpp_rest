93228788f30de454109566e0fe0b8964a0071811
Phil <CppPhil@users.noreply.github.com>
2017-10-01 12:32:01 +0200

    Updated .travis.yml; added travis build scripts

---
+++ scripts/ci/travis/setup_restbed.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+git clone --recursive https://github.com/corvusoft/restbed.git ~
+mkdir ~/restbed/build
+cd ~/restbed/build
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO .
+make install
+
