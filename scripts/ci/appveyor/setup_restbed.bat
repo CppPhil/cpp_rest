1266eab1f6846daa6394b92cc575bb7340e8be20
Phil <CppPhil@users.noreply.github.com>
2017-10-01 14:17:22 +0200

    And another appveyor attempt

--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -5,4 +5,5 @@ mkdir "%HOMEPATH%\restbed\build"
 cd "%HOMEPATH%\restbed\build"
 cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -G "Visual Studio 14" ..
 msbuild /m /p:Configuration=Debug restbed.sln
+cd "C:\cpp_rest"
 
