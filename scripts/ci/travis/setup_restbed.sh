ed751447e379a9fce5b27189c498002310a6ebe4
Phil <CppPhil@users.noreply.github.com>
2017-10-01 12:51:06 +0200

    Fixed setup_restbed.sh

--- scripts/ci/travis/setup_restbed.sh
+++ scripts/ci/travis/setup_restbed.sh
@@ -3,6 +3,6 @@
 git clone --recursive https://github.com/corvusoft/restbed.git ~/restbed
 mkdir ~/restbed/build
 cd ~/restbed/build
-cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO .
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO ..
 make install
 
