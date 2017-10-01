611110218197e26ba946797a7a61d84c0981d109
Phil <CppPhil@users.noreply.github.com>
2017-10-01 14:59:55 +0200

    Updated setup_restbed.bat'

--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -3,7 +3,7 @@
 git clone --recursive https://github.com/corvusoft/restbed.git "%HOMEPATH%\restbed"
 mkdir "%HOMEPATH%\restbed\build"
 cd "%HOMEPATH%\restbed\build"
-cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -G "Visual Studio 14" ..
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -G "Visual Studio 15" ..
 msbuild /m /p:Configuration=Debug restbed.sln
 cd "C:\cpp_rest"
 
