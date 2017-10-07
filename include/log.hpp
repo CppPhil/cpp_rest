0f97945f26c527203df120e92d697e0e545e7417
Phil <CppPhil@users.noreply.github.com>
2017-10-08 00:29:45 +0200

    Added documentation for the log

--- include/log.hpp
+++ include/log.hpp
@@ -1,6 +1,11 @@
+/*!
+ * \file log.hpp
+ * \brief Exports the CR_LOG macro that is to be used to write to
+ *        the application's log file.
+**/
 #ifndef INCG_CRS_LOG_HPP
 #define INCG_CRS_LOG_HPP
-#include "../include/except.hpp" // CR_THROW_IF_NULL
+#include "../include/except.hpp" // CR_THROW_IF_NULL, cr::NullPointerException
 #include <boost/current_function.hpp> // BOOST_CURRENT_FUNCTION
 #include <boost/noncopyable.hpp> // boost::noncopyable
 #include <mutex> // std::mutex
@@ -8,6 +13,13 @@
 #include <fstream> // std::ofstream
 #include <string> // std::string
 #include <utility> // std::forward
+
+/*!
+ * \def CR_LOG(loglevel)
+ * \brief Macro to write to the application's log file using operator<<.
+ *        The parameter must be an enumerator of the cr::LogLevel scoped enum.
+ * \example CR_LOG(LogLevel::Error) << "An error occurred";
+**/
 
 #define CR_LOG(loglevel) \
     ::cr::Log::getInstance().log(loglevel) \
@@ -18,6 +30,11 @@
 
 namespace cr
 {
+/*!
+ * \brief Scoped enum type of the different enumerators representing the
+ *        different log levels that may be used as arguments to the
+ *        CR_LOG macro.
+**/
 enum class LogLevel
 {
     debug,
@@ -28,53 +45,114 @@ enum class LogLevel
     fatal
 };
 
-class Log;
+class Log; // forward declaration of type Log, needed by WriteToLogProxy.
 
+/*!
+ * \brief Type which serves as the result type of the expression that CR_LOG
+ *        expands to. This type is used to write to the application's log
+ *        file through CR_LOG. Locks the mutex for the Log singleton
+ *        in the constructor and unlocks it in the destructor.
+ *        Used to synchronize access to the shared log file handle
+ *        by having an instance of this type lock the shared mutex by creating
+ *        it from the expression that CR_LOG expands to and having it unlock
+ *        the mutex at the end of the expression.
+**/
 class WriteToLogProxy
     : private boost::noncopyable
 {
 public:
     using this_type = WriteToLogProxy;
 
+    /*!
+     * \brief Creates an object of type WriteToLogProxy.
+     *        Locks the mutex of the Log passed into the parameter.
+     * \param log A reference to the Log singleton object.
+     * \note This constructor is called by Log::log.
+    **/
     explicit WriteToLogProxy(Log &log);
 
+    /*!
+     * \brief Move constructs an object of type WriteToLogProxy.
+     *        Takes the pointers from 'other' und nulls out 'other''s pointers.
+     * \param other The object to move construct from.
+    **/
     WriteToLogProxy(this_type &&other);
 
+    /*!
+     * \brief Destroys a WriteToLogProxy.
+     *        Writes two new line characters to the log file, flushes
+     *        the log file causing it to be written out to the harddrive
+     *        and unlocks the Log's mutex if this WriteToLogProxy is not
+     *        a moved-from object.
+    **/
     ~WriteToLogProxy();
 
+    /*!
+     * \brief Prints a printable object to the log file.
+     * \param proxy A reference to the current WriteToLogProxy prvalue
+     *              that exists for the duration of the CR_LOG expression.
+     * \param ty The object to print.
+     * \throws cr::NullPointerException if the WriteToLogProxy is a moved-from
+     *         object.
+    **/
     template <typename Ty>
     friend const WriteToLogProxy &operator<<(const WriteToLogProxy &proxy,
                                              Ty &&ty)
     {
+        // the pointer can't be nullptr!
         CR_THROW_IF_NULL(proxy.m_ofstream);
 
+        // print it.
         (*(proxy.m_ofstream)) << std::forward<Ty>(ty);
         return proxy;
     }
 
 private:
-    std::ofstream *m_ofstream;
-    std::mutex *m_mu;
+    std::ofstream *m_ofstream; /*!< Pointer to the ofstream of the Log singleton */
+    std::mutex *m_mu; /*!< Pointer of the shared mutex inside of the Log singleton */
 };
 
+/*!
+ * \brief The underlying Log type used by CR_LOG.
+**/
 class Log
     : private boost::noncopyable
 {
 public:
     using this_type = Log;
 
-    friend WriteToLogProxy;
+    friend WriteToLogProxy; // befriend WriteToLogProxy so it can fetch its pointers.
 
+    /*!
+     * \brief Returns a reference to the Log singleton object.
+     * \return A reference to the Log singleton object.
+    **/
     static this_type &getInstance();
 
+    /*!
+     * \brief Function called by the CR_LOG macro.
+     *        Creates a WriteToLogProxy object, writes the text for the
+     *        given LogLevel to the log file and then returns the WriteToLogProxy.
+     * \param logLevel The LogLevel to use.
+     * \return The WriteToLogProxy object created.
+    **/
     WriteToLogProxy log(LogLevel logLevel);
 
 private:
+    /*!
+     * \brief Constructor of the Log type.
+     *        Creates an object of type Log.
+     *        Enables exceptions on the underlying filehandle to the log file.
+     * \param logfilePath The path of the log file to write to.
+     * \throws std::ios_base::failure if the ofstream was not in a good state
+     *         after having tried to open the file (Most likely the file
+     *         could not be created / opened).
+    **/
     explicit Log(std::string logfilePath);
 
-    std::string m_logfilePath;
-    std::ofstream m_ofstream;
-    std::mutex m_mu;
+    std::string m_logfilePath; /*!< The string passed into the constructor */
+    std::ofstream m_ofstream; /*!< The underlying handle to the log file */
+    std::mutex m_mu; /*!< The shared mutex used by the WriteToLogProxy objects */
 };
 } // namespace cr
 #endif // INCG_CRS_LOG_HPP
--- src/log.cpp
+++ src/log.cpp
@@ -5,8 +5,21 @@ namespace cr
 {
 namespace
 {
+/*!
+ * \brief The path of the application's log file.
+ *        A relative path.
+ *        The log file will be created in the directory the executable
+ *        is located in.
+**/
 constexpr char logfilePath[] = "cpp_rest.log";
 
+/*!
+ * \brief Module local function to get a C-Style string corresponding to the
+ *        LogLevel passed in.
+ * \param logLevel The loglevel to get a C-Style string for.
+ * \return The null-terminated byte character string corresponding to the
+ *         LogLevel passed in.
+**/
 const char *loglevelAsStr(LogLevel logLevel)
 {
     switch (logLevel) {
@@ -22,7 +35,7 @@ const char *loglevelAsStr(LogLevel logLevel)
         return "[WARNING] ";
     case LogLevel::fatal:
         // FALLTHROUGH
-    default:
+    default: // an invalid value is considered 'fatal'.
         break;
     }
 
@@ -31,36 +44,51 @@ const char *loglevelAsStr(LogLevel logLevel)
 } // anonymous namespace
 
 WriteToLogProxy::WriteToLogProxy(Log &log)
-    : m_ofstream{ &(log.m_ofstream) },
+    : m_ofstream{ &(log.m_ofstream) }, // fetch the pointers.
       m_mu{ &(log.m_mu) }
 {
-    m_mu->lock();
+    m_mu->lock(); // lock the mutex. (It will be unlocked in the dtor.)
 }
 
-WriteToLogProxy::WriteToLogProxy(this_type &&other)
-    : m_ofstream{ nullptr },
+WriteToLogProxy::WriteToLogProxy(this_type &&other) // move constructor
+    : m_ofstream{ nullptr }, // start as nullptr.
       m_mu{ nullptr }
 {
+    // swap the nullptrs with the ones that other holds.
     std::swap(m_ofstream, other.m_ofstream);
     std::swap(m_mu, other.m_mu);
 }
 
 WriteToLogProxy::~WriteToLogProxy()
 {
+    // if this is not a moved from object put newlines into the log file
+    // to end this log entry and flush it to disk.
     if (m_ofstream != nullptr) {
-        (*m_ofstream) << "\n\n" << std::flush;
+        (*m_ofstream) << "\n\n"
+                      << std::flush; // writing the data out to the file like this
+                                     // after every log entry is a significant
+                                     // performance overhead. This however makes
+                                     // debugging much easier as the application
+                                     // is designed to effectively run forever
+                                     // waiting for requests to come in.
     }
 
+    // if this is a moved from object -> return. There is no mutex to unlock.
     if (m_mu == nullptr) {
         return;
     }
 
+    // Unlock the mutex as this object goes out of scope.
+    // (At the end of the expression formed with CR_LOG)
     m_mu->unlock();
 }
 
 Log &Log::getInstance()
 {
+    // Create the Log with static storage duration
     static Log log{ logfilePath };
+
+    // return a reference to the log instance.
     return log;
 }
 
@@ -76,12 +104,14 @@ Log::Log(std::string logfilePath)
       m_ofstream{ m_logfilePath },
       m_mu{ }
 {
+    // if the ofstream is not ok -> throw.
     if (not m_ofstream) {
         throw std::ios_base::failure{
             m_logfilePath + " is not a valid path for the log"
         };
     }
 
+    // Enable exceptions for the filestream.
     m_ofstream.exceptions(std::ofstream::failbit | std::ofstream::badbit
                           | std::ofstream::eofbit);
 }
