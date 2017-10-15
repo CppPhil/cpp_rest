c4c1d81a231ee691e2a7332c622b0698302c7c2d
Phil <CppPhil@users.noreply.github.com>
2017-10-15 14:19:28 +0200

    trying to make travis work

--- scripts/dependencies/linux/install_mysql.sh
+++ scripts/dependencies/linux/install_mysql.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo apt-get -y install mysql-server-core-5.5
-sudo apt-get -y install mysql-client-5.5
+sudo apt-get -y install mysql-server-core-5.6
+sudo apt-get -y install mysql-client-5.6
 export DEBIAN_FRONTEND="noninteractive"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
-sudo apt-get -y install mysql-server-5.5
+sudo apt-get -y install mysql-server-5.6
 sudo chmod -R 755 /val/lib/mysql/
-sudo apt-get install libmysqlclient-dev
+sudo apt-get -y install libmysqlclient-dev
 sudo service mysql restart
 mysql --user=root --password=mysql < ./scripts/sql/setup_db.sql
 
