47ca6d74c1fe2b192be2a57b43277e4a254ff663
Phil <CppPhil@users.noreply.github.com>
2017-10-15 21:54:46 +0200

    eaonetahoutn

--- scripts/ci/appveyor/setup_cppdb.bat
+++ scripts/ci/appveyor/setup_cppdb.bat
@@ -7,7 +7,7 @@ mkdir "%USERPROFILE%\cppdb\build"
 
 cd "%USERPROFILE%\cppdb\build"
 
-cmake -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
+cmake -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="OFF" -G "Visual Studio 14" ..
 cmake --build .
 
 xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest" /Y
