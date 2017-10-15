e32b6ef560dceb8b5e4fdadf1a65ec77688dc659
Phil <CppPhil@users.noreply.github.com>
2017-10-15 16:10:17 +0200

    au

--- scripts/ci/travis/setup_cppdb.sh
+++ scripts/ci/travis/setup_cppdb.sh
@@ -6,11 +6,11 @@ mkdir ~/cppdb/build
 cd ~/cppdb/build
 cmake -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" ..
 make
-make install
-cp ~/cppdb/build/libcppdb.so /usr/local/libcppdb.so
-cp ~/cppdb/build/libcppdb_mysql.so /usr/local/libcppdb_mysql.so
-cp ~/cppdb/build/libcppdb.so /usr/lib/libcppdb.so
-cp ~/cppdb/build/libcppdb_mysql.so /usr/lib/libcppdb_mysql.so
-cp ~/cppdb/build/libcppdb.so ~/build/CppPhil/cpp_rest/libcppdb.so
-cp ~/cppdb/build/libcppdb_mysql.so ~/build/CppPhil/cpp_rest/libcppdb_mysql.so
+sudo make install
+sudo cp ~/cppdb/build/libcppdb.so /usr/local/libcppdb.so
+sudo cp ~/cppdb/build/libcppdb_mysql.so /usr/local/libcppdb_mysql.so
+sudo cp ~/cppdb/build/libcppdb.so /usr/lib/libcppdb.so
+sudo cp ~/cppdb/build/libcppdb_mysql.so /usr/lib/libcppdb_mysql.so
+sudo cp ~/cppdb/build/libcppdb.so ~/build/CppPhil/cpp_rest/libcppdb.so
+sudo cp ~/cppdb/build/libcppdb_mysql.so ~/build/CppPhil/cpp_rest/libcppdb_mysql.so
 
