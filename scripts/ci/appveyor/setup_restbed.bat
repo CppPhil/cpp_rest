dc9d2622f32bfd71f306e8a61410eccad71d4e4b
Phil <CppPhil@users.noreply.github.com>
2017-10-15 22:18:45 +0200

    Another test

--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -7,3 +7,5 @@ cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=YES -G 
 msbuild /m /p:Configuration=Debug restbed.sln
 cd "C:\cpp_rest"
 
+xcopy "%USERPROFILE%\restbed\build\Debug\restbed.dll" "C:\cpp_rest\Debug" /Y
+
