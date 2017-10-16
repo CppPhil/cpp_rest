5da8a09409cf83d4fbd35461ea87b2b2cef73e1c
Phil <CppPhil@users.noreply.github.com>
2017-10-16 19:31:34 +0200

    test

--- scripts/ci/appveyor/setup_cppdb.bat
+++ scripts/ci/appveyor/setup_cppdb.bat
@@ -11,9 +11,9 @@ move /y "C:\cpp_rest\cppdb_windows_cmakelists\CMakeLists.txt" "%USERPROFILE%\cpp
 
 mkdir "%USERPROFILE%\cppdb\build\Debug"
 
-xcopy "%USERPROFILE%\mysql-connector-c-6.1.11-winx64\lib\libmysql.dll" "%USERPROFILE%\cppdb\build\Debug" /Y
+xcopy "%USERPROFILE%\mysql-connector-c-6.1.11-win32\lib\libmysql.dll" "%USERPROFILE%\cppdb\build\Debug" /Y
 
-cmake -DCMAKE_PREFIX_PATH="%USERPROFILE%\mysql-connector-c-6.1.11-winx64" -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
+cmake -DCMAKE_PREFIX_PATH="%USERPROFILE%\mysql-connector-c-6.1.11-win32" -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
 cmake --build .
 
 xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest" /Y
