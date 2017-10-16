38bd03428e19311c7fa2507a080da3ce07dc652a
Phil <CppPhil@users.noreply.github.com>
2017-10-16 17:25:21 +0200

    Switching back to static linking for Windows

--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -3,7 +3,7 @@
 git clone --recursive https://github.com/corvusoft/restbed.git "%USERPROFILE%\restbed"
 mkdir "%USERPROFILE%\restbed\build"
 cd "%USERPROFILE%\restbed\build"
-cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=YES -G "Visual Studio 14" ..
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -G "Visual Studio 14" ..
 msbuild /m /p:Configuration=Debug restbed.sln
 cd "C:\cpp_rest"
 
--- src/database_session.cpp
+++ src/database_session.cpp
@@ -1,5 +1,19 @@
 #include "../include/database_session.hpp"
+#include "../include/os.hpp" // CR_OS, CR_OS_WINDOWS
+#include <cppdb/backend.h> // cppdb::backend::connection
+#include <cppdb/driver_manager.h> // cppdb::driver_manager
 #include <utility> // std::move
+#if CR_OS == CR_OS_WINDOWS
+#   include <mutex> // std::call_once, std::once_flag
+#endif // CR_OS == CR_OS_WINDOWS
+
+#if CR_OS == CR_OS_WINDOWS
+extern "C"
+{
+cppdb::backend::connection *cppdb_mysql_get_connection(
+    const cppdb::connection_info &);
+} // extern "C"
+#endif // CR_OS == CR_OS_WINDOWS
 
 namespace cr
 {
@@ -7,6 +21,22 @@ DataBaseSession::~DataBaseSession() = default;
 
 cppdb::session &DataBaseSession::getInstance()
 {
+#if CR_OS == CR_OS_WINDOWS
+    static std::once_flag onceFlag{ };
+
+    std::call_once(
+        onceFlag,
+        [] {
+            cppdb::driver_manager::instance().install_driver(
+                "mysql",
+                new cppdb::backend::static_driver{
+                    &cppdb_mysql_get_connection
+                }
+            );
+        }
+    );
+#endif // CR_OS == CR_OS_WINDOWS
+
     // For documentation of this MySQL connection string, see:
     // http://cppcms.com/sql/cppdb/classcppdb_1_1session.html#a7ae75affbf1b2e809250a6e2ff36bf2c
     static constexpr char connectionString[]
