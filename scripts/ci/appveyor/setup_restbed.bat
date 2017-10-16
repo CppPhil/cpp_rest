604f365c1ffd499861fa6ccb767401e5d036c271
Phil <CppPhil@users.noreply.github.com>
2017-10-16 21:24:15 +0200

    test

--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -3,7 +3,7 @@
 git clone --recursive https://github.com/corvusoft/restbed.git "%USERPROFILE%\restbed"
 mkdir "%USERPROFILE%\restbed\build"
 cd "%USERPROFILE%\restbed\build"
-cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=YES -DCMAKE_BUILD_TYPE="Release" -G "Visual Studio 14" ..
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=YES -G "Visual Studio 14" ..
 msbuild /m /p:Configuration=Release restbed.sln
 
 mkdir "C:\cpp_rest\Release"
@@ -11,4 +11,12 @@ mkdir "C:\cpp_rest\Release"
 xcopy "%USERPROFILE%\restbed\build\Release\restbed.dll" "C:\cpp_rest\Release" /Y
 
 cd "C:\cpp_rest"
+
+ECHO ">>>>>>>>>>>>>>!!!!!!HEREHEREHERE<<<<<<<<<<<<<<<<<<!!!!!!!!!!"
+
+dir "%USERPROFILE%\restbed"
+
+dir "%USERPROFILE%\restbed\build"
+
+dir "%USERPROFILE%\restbed\build\Release"
 
