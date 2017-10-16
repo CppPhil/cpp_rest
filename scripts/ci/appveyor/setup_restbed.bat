20ab881273a65ed540e2d5e55bff982d20e1b691
Phil <CppPhil@users.noreply.github.com>
2017-10-16 22:10:57 +0200

    All the stuff needs to be /MT / /MTd on windows; there's some CMake script in restbed that forces /MT / /MTd.....

--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -3,24 +3,6 @@
 git clone --recursive https://github.com/corvusoft/restbed.git "%USERPROFILE%\restbed"
 mkdir "%USERPROFILE%\restbed\build"
 cd "%USERPROFILE%\restbed\build"
-cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=YES -G "Visual Studio 14" ..
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -G "Visual Studio 14" ..
 msbuild /m /p:Configuration=Release restbed.sln
-
-mkdir "C:\cpp_rest\Release"
-
-xcopy "%USERPROFILE%\restbed\build\Release\restbed.dll" "C:\cpp_rest\Release" /Y
-
-xcopy "%USERPROFILE%\restbed\build\Release\restbed.dll" "C:\WINDOWS\System32" /Y
-
-xcopy "%USERPROFILE%\restbed\build\Release\restbed.dll" "C:\WINDOWS\SysWoW64" /Y
-
-ECHO ">>>>>>>>>>>!!!! HERE HERE HERE !!!!!<<<<<<<<<<"
-
-dir "%USERPROFILE%\restbed\build"
-
-dir "%USERPROFILE%\restbed\build\Release"
-
-dir "%USERPROFILE%\restbed\build\Win32"
-
-dir "%USERPROFILE%\restbed\build\Win32\Release"
 
