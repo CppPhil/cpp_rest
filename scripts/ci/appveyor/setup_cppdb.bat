404d8c86a5bf580caf52561f7d06b33e35d0da56
Phil <CppPhil@users.noreply.github.com>
2017-10-16 18:23:03 +0200

    tneuho

--- scripts/ci/appveyor/setup_cppdb.bat
+++ scripts/ci/appveyor/setup_cppdb.bat
@@ -8,7 +8,7 @@ mkdir "%USERPROFILE%\cppdb\build"
 cd "%USERPROFILE%\cppdb\build"
 
 cmake -DCMAKE_PREFIX_PATH="%USERPROFILE%\mysql-connector-c-6.1.11-winx64" -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
-cmake --build . -- /p:NOMINMAX /p:_CRT_SECURE_NO_WARNINGS /p:_SCL_SECURE_NO_WARNINGS
+cmake --build . -- /DNOMINMAX /D_CRT_SECURE_NO_WARNINGS /D_SCL_SECURE_NO_WARNINGS
 
 xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest" /Y
 xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest\Debug" /Y
