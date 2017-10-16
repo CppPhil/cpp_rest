511eb182b03e5a6b3eea18d7daef12884872b7aa
Phil <CppPhil@users.noreply.github.com>
2017-10-16 19:43:25 +0200

    test

--- scripts/ci/appveyor/setup_cppdb.bat
+++ scripts/ci/appveyor/setup_cppdb.bat
@@ -9,13 +9,14 @@ cd "%USERPROFILE%\cppdb\build"
 
 move /y "C:\cpp_rest\cppdb_windows_cmakelists\CMakeLists.txt" "%USERPROFILE%\cppdb\CMakeLists.txt"
 
-mkdir "%USERPROFILE%\cppdb\build\Debug"
+mkdir "%USERPROFILE%\cppdb\build\Release"
 
-xcopy "%USERPROFILE%\mysql-connector-c-6.1.11-win32\lib\libmysql.dll" "%USERPROFILE%\cppdb\build\Debug" /Y
+xcopy "%USERPROFILE%\mysql-connector-c-6.1.11-win32\lib\libmysql.dll" "%USERPROFILE%\cppdb\build\Release" /Y
 
-cmake -DCMAKE_PREFIX_PATH="%USERPROFILE%\mysql-connector-c-6.1.11-win32" -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
+cmake -DCMAKE_PREFIX_PATH="%USERPROFILE%\mysql-connector-c-6.1.11-win32" -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -DCMAKE_BUILD_TYPE="Release" -G "Visual Studio 14" ..
 cmake --build .
 
-xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest" /Y
-xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest\Debug" /Y
+xcopy "%USERPROFILE%\cppdb\build\Release\cppdb.dll" "C:\cpp_rest" /Y
+mkdir "C:\cpp_rest\Release"
+xcopy "%USERPROFILE%\cppdb\build\Release\cppdb.dll" "C:\cpp_rest\Release" /Y
 
--- scripts/ci/appveyor/setup_restbed.bat
+++ scripts/ci/appveyor/setup_restbed.bat
@@ -3,9 +3,7 @@
 git clone --recursive https://github.com/corvusoft/restbed.git "%USERPROFILE%\restbed"
 mkdir "%USERPROFILE%\restbed\build"
 cd "%USERPROFILE%\restbed\build"
-cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -G "Visual Studio 14" ..
-msbuild /m /p:Configuration=Debug restbed.sln
+cmake -DBUILD_TESTS=NO -DBUILD_EXAMPLES=NO -DBUILD_SSL=NO -DBUILD_SHARED=NO -DCMAKE_BUILD_TYPE="Release" -G "Visual Studio 14" ..
+msbuild /m /p:Configuration=Release restbed.sln
 cd "C:\cpp_rest"
-
-mkdir "C:\cpp_rest\Debug"
 
