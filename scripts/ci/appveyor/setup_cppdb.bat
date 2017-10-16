a1084cb047c8d19567bf77981c532610719da9d8
Phil <CppPhil@users.noreply.github.com>
2017-10-16 17:45:27 +0200

    another attempt to make appveyor work

--- scripts/ci/appveyor/setup_cppdb.bat
+++ scripts/ci/appveyor/setup_cppdb.bat
@@ -1,15 +1,5 @@
 @echo off
 
-REM download MySQL Client Library .zip file
-bitsadmin.exe /transfer "MySQL Client Library" "https://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.11-winx64.zip" "%USERPROFILE%"
-
-REM download unzip utility
-bitsadmin.exe /transfer "unzip" "http://stahlworks.com/dev/unzip.exe" "%USERPROFILE%"
-
-REM unzip the .zip file into the current working directory
-cd "%USERPROFILE%"
-"%USERPROFILE%\unzip.exe" "%USERPROFILE%\mysql-connector-c-6.1.11-winx64.zip"
-
 REM install cppdb
 svn checkout --quiet http://cppcms.svn.sourceforge.net/svnroot/cppcms/cppdb/trunk "%USERPROFILE%\cppdb"
 
@@ -17,7 +7,7 @@ mkdir "%USERPROFILE%\cppdb\build"
 
 cd "%USERPROFILE%\cppdb\build"
 
-cmake -DCMAKE_PREFIX_PATH="%USERPROFILE%\mysql-connector-c-6.1.11-winx64" -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
+cmake -DDISABLE_SQLITE="ON" -DDISABLE_PQ="ON" -DDISABLE_ODBC="ON" -DDISABLE_MYSQL="OFF" -DMYSQL_BACKEND_INTERNAL="ON" -G "Visual Studio 14" ..
 cmake --build .
 
 xcopy "%USERPROFILE%\cppdb\build\Debug\cppdb.dll" "C:\cpp_rest" /Y
