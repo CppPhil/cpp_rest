3d8804476208c4324c5954fe83d4879bf0e4c6c3
Phil <CppPhil@users.noreply.github.com>
2017-10-18 20:23:21 +0200

    Changes to hopefully make it work on GNU/Linux

--- scripts/dependencies/linux/install_mysql.sh
+++ scripts/dependencies/linux/install_mysql.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-sudo apt-get -y install mysql-server-core-5.6
-sudo apt-get -y install mysql-client-5.6
+sudo apt-get -y install mysql-client
 export DEBIAN_FRONTEND="noninteractive"
 debconf-set-selections <<< "mysql-server mysql-server/root_password password mysql"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password mysql"
-sudo apt-get -y install mysql-server-5.6
-sudo apt-get -y install libmysqlclient-dev
+sudo apt-get -y install mysql-server
+sudo apt-get -y install default-libmysqlclient-dev
 sudo service mysql restart
 mysql --user=root --password="mysql" < ./scripts/sql/setup_db.sql
 
