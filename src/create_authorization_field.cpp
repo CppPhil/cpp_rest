764a2ec85a8534ae41a4cf30c36017a3ab299734
Phil <CppPhil@users.noreply.github.com>
2017-11-13 13:44:31 +0100

    ueaou

--- src/create_authorization_field.cpp
+++ src/create_authorization_field.cpp
@@ -1,5 +1,4 @@
 #include "../include/create_authorization_field.hpp"
-#include <boost/archive/iterators/insert_linebreaks.hpp> // boost::archive::iterators::insert_linebreaks
 #include <boost/archive/iterators/base64_from_binary.hpp> // boost::archive::iterators::base64_from_binary
 #include <boost/archive/iterators/transform_width.hpp> // boost::archive::iterators::transform_width
 #include <boost/archive/iterators/ostream_iterator.hpp> // boost::archive::iterators::ostream_iterator
@@ -24,17 +23,13 @@ std::string joinWithColon(
 std::string toBase64(boost::string_ref input)
 {
     namespace ait = boost::archive::iterators;
-    static constexpr int lineBreakAfter = 72; // break after 72 chars
     static constexpr int base64Bits     =  6;
     static constexpr int byteBits       =  8;
 
     static_assert(byteBits == CHAR_BIT, "Bytes are not 8 bits!");
 
-    using Base64
-        = ait::insert_linebreaks<
-            ait::base64_from_binary<
-                ait::transform_width<const char *, base64Bits, byteBits>
-            >, lineBreakAfter>;
+    using Base64 = ait::base64_from_binary<
+        ait::transform_width<const char *, base64Bits, byteBits>>;
 
     std::ostringstream os{ };
 
